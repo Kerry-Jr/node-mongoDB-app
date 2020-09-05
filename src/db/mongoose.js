2d32f10349c2329595a54e867ec1b777ea8f98d6
Kerry Smith <kerrysfs@gmail.com>
2020-09-04 21:17:18 -0700

    more testing saving to db, added some validations that are built into Mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,37 +8,72 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+        if(!validator.isEmail(value)) {
+          throw new Error('Email is invalid')
+        }
+
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if(value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"')
+      }
+    }
   }
 })
 
 const Task = mongoose.model('Task', {
   description: {
-    type: String
+    type: String,
+    trim: true,
+    required: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 })
 
 
-const me = new User({
-  name: 'Kerry',
-  age: 35
-})
-
-me.save().then((me) => {
-  console.log(me)
-}).catch((error) => {
-  console.log('Error!', error)
-})
+// const me = new User({
+//   name: '         Merry              ',
+//   email: 'MERRY@GMAIL.COM         ',
+//   password: '1265327Ksq!'
+// })
+//
+// me.save().then((me) => {
+//   console.log(me)
+// }).catch((error) => {
+//   console.log('Error!', error)
+// })
 
 
 const task = new Task({
-  description: 'Learn the mongoose Library',
+  description: '                  Get a good programming job and stop be lazy',
   completed: false
 })
 task.save().then(() => {
