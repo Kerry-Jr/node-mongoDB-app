84a4e542f91374e374214642fd545d69d81678e4
Kerry Smith <kerrysfs@gmail.com>
2020-09-08 23:04:44 -0700

    user login validators implemented and endpoint created

--- playground/compare-model-user.js
+++ playground/compare-model-user.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const validator = require("validator");
 const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
@@ -16,9 +16,18 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid')
+        throw new Error("Email is invalid");
       }
-    }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
   },
   password: {
     type: String,
@@ -26,49 +35,38 @@ const userSchema = new mongoose.Schema({
     minlength: 7,
     trim: true,
     validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"')
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
       }
-    }
+    },
   },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a postive number')
-      }
-    }
-  }
-})
+});
 
 userSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({ email })
-
-  if (!user) {
+  const user = User.findOne({ email })
+  if(!user){
     throw new Error('Unable to login')
   }
-
   const isMatch = await bcrypt.compare(password, user.password)
 
-  if (!isMatch) {
+  if(!isMatch){
     throw new Error('Unable to login')
   }
-
   return user
 }
 
-// Hash the plain text password before saving
-userSchema.pre('save', async function (next) {
+// has the plain text password b4 saving
+userSchema.pre("save", async function (next) {
   const user = this
 
-  if (user.isModified('password')) {
+  if(user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8)
+
   }
 
   next()
-})
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
