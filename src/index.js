0d5eb7f17f986788d6e645fdeb20af3e93c2f8e5
Kerry Smith <kerrysfs@gmail.com>
2020-09-09 22:04:59 -0700

    setup express middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
@@ -8,6 +9,22 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+
+//express middleware
+// app.use((req, res, next) => {
+// if(req.method === 'GET') {
+//   res.send('GET requests are disabled')
+// }else {
+//   next()
+// }
+// })
+
+
+app.use((req, res, next) => {
+  res.status(503).send('Sorry site is down for bug zapping and upgrading rocket boosters ')
+})
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -23,21 +40,47 @@ app.listen(port, () => {
 
 
 
-const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 
 const myFunction = async () => {
-  const password = 'Red12345!'
-  const hashedPassword = await bcrypt.hash(password,8)
 
-  console.log(password)
-  console.log(hashedPassword)
+const token = jwt.sign({ _id: '123abc' }, 'thisismynewcourse', {
+  expiresIn: '7 days'
+})
 
-  const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-  console.log(isMatch)
+console.log(token)
 
+const data =  jwt.verify(token, 'thisismynewcourse')
+
+console.log(data)
 
 }
 
 myFunction()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this was the example for use in the myFunction() for bcrypt password hashing
+// const password = 'Red12345!'
+// const hashedPassword = await bcrypt.hash(password,8)
+//
+// console.log(password)
+// console.log(hashedPassword)
+//
+// const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+// console.log(isMatch)
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -39,9 +40,33 @@ const userSchema = new mongoose.Schema({
         throw new Error('Age must be a postive number')
       }
     }
-  }
+  },
+  tokens: [{
+   token: {
+     type: String,
+     required: true
+   }
+
+  }]
 })
 
+//our methods --look below--- are available on the instances (example for instance - the user model, task model)
+
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({_id: user._id.toString()}, 'thisismynewcourse')
+
+   user.tokens = user.tokens.concat({ token })
+
+  await user.save();
+
+  return token
+}
+
+
+
+//statics methods are accessible on the model sometimes called model methods
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email })
 
