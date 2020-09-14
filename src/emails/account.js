2e76ff28010b442c9379249948ae2faea0eac95a
Kerry Smith <kerrysfs@gmail.com>
2020-09-13 22:43:37 -0700

    quick change

--- src/emails/account.js
+++ src/emails/account.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: process.env.EMAIL_ADDRESS,
+    from: 'kerrysfs@gmail.com',
     subject: "Welcome to the project",
     text: `Welcome to the experience ${name} Let me know how you like the app. Thanks!`,
     // html: '<h1>HELLO, Thanks for Joining!</h1>' this can easily be added
@@ -16,7 +16,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendTerminationEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: process.env.EMAIL_ADDRESS,
+    from: 'kerrysfs@gmail.com',
     subject: "A little birdie is telling me you cancelled your account...",
     text: `Hate to see you go and I'm sure you have your reasons ${name}, anyway you could let us know whats up and how we might be able to provide better service in the future?  Feel free to respond to this email ${name}.  Thank you and no hard feelings..lol yea we just sold your ALL of your data!!! hahah`,
   });
